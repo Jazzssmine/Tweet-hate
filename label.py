87a4a8c7b8182f4d6c7082de8ca3800ec92e5b53
jialuoh2 <55225419+jialuoh2@users.noreply.github.com>
2023-11-26 17:55:04 -0600

    Add files via upload

---
+++ label.py
@@ -0,0 +1,155 @@
+import torch
+import numpy as np
+from transformers import BertTokenizer
+tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
+labels = {'attacker': 0,
+          'bystander': 1,
+          'supporter': 2,
+          np.nan: 9000
+          }
+
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self, df):
+        self.labels = [labels[label] for label in df['labels']]
+        self.texts = [tokenizer(text, 
+                                padding='max_length', 
+                                max_length = 512, 
+                                truncation=True,
+                                return_tensors="pt") 
+                      for text in df['text']]
+
+    def classes(self):
+        return self.labels
+
+    def __len__(self):
+        return len(self.labels)
+
+    def get_batch_labels(self, idx):
+        # Fetch a batch of labels
+        return np.array(self.labels[idx])
+
+    def get_batch_texts(self, idx):
+        # Fetch a batch of inputs
+        return self.texts[idx]
+
+    def __getitem__(self, idx):
+        batch_texts = self.get_batch_texts(idx)
+        batch_y = self.get_batch_labels(idx)
+        return batch_texts, batch_y
+
+from torch import nn
+from transformers import BertModel
+
+class BertClassifier(nn.Module):
+    def __init__(self, dropout=0.5):
+        super(BertClassifier, self).__init__()
+        self.bert = BertModel.from_pretrained('bert-base-cased')
+        self.dropout = nn.Dropout(dropout)
+        self.linear = nn.Linear(768, 3)
+        self.relu = nn.ReLU()
+
+    def forward(self, input_id, mask):
+        _, pooled_output = self.bert(input_ids= input_id, attention_mask=mask,return_dict=False)
+        dropout_output = self.dropout(pooled_output)
+        linear_output = self.linear(dropout_output)
+        final_layer = self.relu(linear_output)
+        return final_layer
+    
+from torch.optim import Adam
+from tqdm import tqdm
+
+def train(model, train_data, val_data, learning_rate, epochs):
+    train, val = Dataset(train_data), Dataset(val_data)
+    train_dataloader = torch.utils.data.DataLoader(train, batch_size=2, shuffle=True)
+    val_dataloader = torch.utils.data.DataLoader(val, batch_size=2)
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+    criterion = nn.CrossEntropyLoss()
+    optimizer = Adam(model.parameters(), lr=learning_rate)
+
+    if use_cuda:
+            model = model.cuda()
+            criterion = criterion.cuda()
+    for epoch_num in range(epochs):
+            total_acc_train = 0
+            total_loss_train = 0
+            for train_input, train_label in tqdm(train_dataloader):
+
+                train_label = train_label.to(device)
+                train_label = train_label.type(torch.LongTensor)
+                mask = train_input['attention_mask'].to(device)
+                input_id = train_input['input_ids'].squeeze(1).to(device)
+                output = model(input_id, mask)
+                batch_loss = criterion(output, train_label)
+                total_loss_train += batch_loss.item()
+                acc = (output.argmax(dim=1) == train_label).sum().item()
+                total_acc_train += acc
+                model.zero_grad()
+                batch_loss.backward()
+                optimizer.step()
+            total_acc_val = 0
+            total_loss_val = 0
+            with torch.no_grad():
+                for val_input, val_label in val_dataloader:
+                    val_label = val_label.to(device)
+                    val_label = val_label.type(torch.LongTensor)
+                    mask = val_input['attention_mask'].to(device)
+                    input_id = val_input['input_ids'].squeeze(1).to(device)
+  
+                    output = model(input_id, mask)
+
+                    batch_loss = criterion(output, val_label)
+                    total_loss_val += batch_loss.item()
+                    
+                    acc = (output.argmax(dim=1) == val_label).sum().item()
+                    total_acc_val += acc
+            
+            print(
+                f'''Epochs: {epoch_num + 1} 
+              | Train Loss: {total_loss_train / len(train_data): .3f} 
+              | Train Accuracy: {total_acc_train / len(train_data): .3f} 
+              | Val Loss: {total_loss_val / len(val_data): .3f} 
+              | Val Accuracy: {total_acc_val / len(val_data): .3f}''')  
+
+
+EPOCHS = 10
+model = BertClassifier()
+LR = 1e-6
+import pandas as pd
+text_df = pd.read_csv('test.csv')
+
+df = pd.DataFrame(text_df)
+
+df_both = df.iloc[:98,:]
+df_train = df_both.iloc[:96,:]
+df_val = df_both.iloc[96:,:]
+df_test = df.iloc[96:,:]
+print(df_train.head())
+print(len(df_train),len(df_val), len(df_test))
+
+train(model, df_train, df_val, LR, EPOCHS)
+
+def test(model, test_data):
+    model.eval()
+    test = Dataset(test_data)
+    test_dataloader = torch.utils.data.DataLoader(test, batch_size=1)
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+    if use_cuda:
+        model = model.cuda()
+
+    total_acc_test = 0
+    with torch.no_grad():
+        i = 0
+        for test_input, test_label in test_dataloader:
+              test_label = test_label.to(device)
+              mask = test_input['attention_mask'].to(device)
+              input_id = test_input['input_ids'].squeeze(1).to(device)
+              output = model(input_id, mask)
+              df_test.at[i, 'labels'] = output.argmax(1)
+              acc = (output.argmax(dim=1) == test_label).sum().item()
+              total_acc_test += acc 
+              i = i + 1
+    df_test.to_csv("test_after.csv", index=False)
+    
+test(model, df_test)
